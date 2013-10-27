8fad4ef446abbcc98bfc9135351ea2802200569a
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-10-27 20:35:53 +0900

    ListFragment << foterView

--- HBFav/src/main/java/com/hbfav/EntryListFragment.java
+++ HBFav/src/main/java/com/hbfav/EntryListFragment.java
@@ -2,49 +2,103 @@ package com.hbfav;
 
 
 import android.app.ListFragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class EntryListFragment extends ListFragment {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class EntryListFragment extends ListFragment implements AbsListView.OnScrollListener {
     private TextView headerTextView;
-    private String[] entries;
+    private View mFooterView;
+    private ArrayList<String> mEntries;
+    private ArrayAdapter<String> mAdapter;
+    private AsyncTask mTask;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        entries = new String[] {
-                "hello world 00",
-                "hello world 01",
-                "hello world 02"
-        };
+        mEntries = new ArrayList<String>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_entry_list_view_main, container, false);
+        headerTextView = (TextView) rootView.findViewById(R.id.list_header_text);
+        mFooterView = inflater.inflate(R.layout.listview_footer, null);
+        return rootView;
+    }
 
-        setListAdapter(new ArrayAdapter<String>(
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ListView listView = getListView();
+        listView.addFooterView(mFooterView);
+        listView.setOnScrollListener(this);
+
+        mAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
-                entries
-        ));
-
-        headerTextView = (TextView) rootView.findViewById(R.id.list_header_text);
-        return rootView;
+                mEntries
+        );
+        setListAdapter(mAdapter);
     }
 
     @Override
     public void onListItemClick(ListView listView, View view, int position, long id) {
         super.onListItemClick(listView, view, position, id);
 
-        headerTextView.setText(entries[position]);
+        headerTextView.setText(mEntries.get(position));
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (totalItemCount == firstVisibleItem + visibleItemCount) {
+            additionalReading();
+        }
+    }
+
+    private void additionalReading() {
+        if (mTask != null && mTask.getStatus() == AsyncTask.Status.RUNNING) {
+            return;
+        }
+        mTask = new AsyncTask<Long, Void, Void>() {
+            @Override
+            protected Void doInBackground(Long[] params) {
+                try {
+                    Thread.sleep(params[0]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            };
+
+            protected void onPostExecute(Void result) {
+                addListData();
+                getListView().invalidateViews();
+            };
+        }.execute(Math.abs(new Random(System.currentTimeMillis()).nextLong() % 3000));
+    }
+
+    private void addListData() {
+        for (int i = 0; i < 10; i++) {
+            mEntries.add("Hello world");
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
