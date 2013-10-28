f299caf230bb1db436a5c889e6baa7b33ea44cb8
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-10-28 20:16:13 +0900

    Add sample activity test

---
+++ HBFav/src/instrumentTest/java/com/hbfav/android/test/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.hbfav.android.test;
+
+
+import android.app.ActionBar;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.hbfav.android.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity mActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        mActivity = getActivity();
+    }
+
+    public void testStringForDisplay() throws Exception {
+        ActionBar actionBar = mActivity.getActionBar();
+        String result = actionBar.getTitle().toString();
+        assertEquals(result, "Section 1");
+    }
+}
