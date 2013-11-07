bafda296358eb550f58a957f5d8010201601ac52
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-11-08 08:53:03 +0900

    Refactor EntryListFeedManager

--- HBFav/src/main/java/com/hbfav/android/controllers/BaseListFeedManager.java
+++ HBFav/src/main/java/com/hbfav/android/controllers/BaseListFeedManager.java
@@ -1,22 +1,87 @@
 package com.hbfav.android.controllers;
 
+import com.hbfav.R;
 import com.hbfav.android.interfaces.FeedResponseHandler;
 import com.hbfav.android.models.Entry;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public abstract class BaseListFeedManager {
-    protected abstract void replaceAll(ArrayList<Entry> entries);
-
-    public abstract void replaceFeed(final FeedResponseHandler feedResponseHandler);
-
-    public abstract void setCategory(int category);
-
-    public abstract void clearList();
+    public abstract void setList(ArrayList<Entry> entries);
 
     public abstract ArrayList<Entry> getList();
 
-    public abstract Entry get(Integer position);
+    public abstract void setCategory(int category);
 
     public abstract int getCategory();
+
+    protected abstract String getEndPoint();
+
+
+    public Entry get(Integer index) {
+        return getList().get(index);
+    }
+
+    public void clearList() {
+        setList(new ArrayList<Entry>());
+    }
+
+    public void replaceFeed(final FeedResponseHandler feedResponseHandler) {
+        String endpoint = getEndPoint();
+        switch (getCategory()) {
+            case R.id.option_category_menu_social:
+                endpoint += "?category=social";
+                break;
+            case R.id.option_category_menu_it:
+                endpoint += "?category=it";
+                break;
+            case R.id.option_category_menu_economics:
+                endpoint += "?category=economics";
+                break;
+            case R.id.option_category_menu_life:
+                endpoint += "?category=life";
+                break;
+            case R.id.option_category_menu_entertainment:
+                endpoint += "?category=entertainment";
+                break;
+            case R.id.option_category_menu_knowledge:
+                endpoint += "?category=knowledge";
+                break;
+            case R.id.option_category_menu_game:
+                endpoint += "?category=geme";
+                break;
+            case R.id.option_category_menu_fun:
+                endpoint += "?category=fun";
+                break;
+        }
+        BookmarksFetcher.get(endpoint, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(JSONObject jObj) {
+                try {
+                    ArrayList<Entry> entries = new ArrayList<Entry>();
+                    JSONArray bookmarkJsons = jObj.getJSONArray("bookmarks");
+                    for (int i = 0; i < bookmarkJsons.length(); i++) {
+                        Entry entry = new Entry((JSONObject) bookmarkJsons.get(i));
+                        entries.add(entry);
+                    }
+                    entries = new ArrayList<Entry>(new LinkedHashSet<Entry>(entries));
+                    setList(entries);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                feedResponseHandler.onSuccess();
+            }
+
+            @Override
+            public void onFinish() {
+                feedResponseHandler.onFinish();
+            }
+        });
+    }
 }
--- HBFav/src/main/java/com/hbfav/android/controllers/EntryListFeedManager.java
+++ HBFav/src/main/java/com/hbfav/android/controllers/EntryListFeedManager.java
@@ -1,13 +1,7 @@
 package com.hbfav.android.controllers;
 
 import com.hbfav.R;
-import com.hbfav.android.interfaces.FeedResponseHandler;
 import com.hbfav.android.models.Entry;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -26,24 +20,13 @@ public class EntryListFeedManager extends BaseListFeedManager {
     }
 
     @Override
-    public void clearList() {
-        bookmarks = null;
-        bookmarks = new ArrayList<Entry>();
-    }
-
-    @Override
-    protected void replaceAll(ArrayList<Entry> entries) {
-        bookmarks = new ArrayList<Entry>(new LinkedHashSet<Entry>(entries));
-    }
-
-    @Override
-    public Entry get(Integer index) {
-        return bookmarks.get(index);
-    }
-
-    @Override
     public ArrayList<Entry> getList() {
         return bookmarks;
+    }
+
+    @Override
+    public void setList(ArrayList<Entry> entries) {
+        this.bookmarks = new ArrayList<Entry>(new LinkedHashSet<Entry>(entries));
     }
 
     @Override
@@ -57,56 +40,7 @@ public class EntryListFeedManager extends BaseListFeedManager {
     }
 
     @Override
-    public void replaceFeed(final FeedResponseHandler feedResponseHandler) {
-        String endpoint = "entrylist";
-        switch (category) {
-            case R.id.option_category_menu_social:
-                endpoint += "?category=social";
-                break;
-            case R.id.option_category_menu_it:
-                endpoint += "?category=it";
-                break;
-            case R.id.option_category_menu_economics:
-                endpoint += "?category=economics";
-                break;
-            case R.id.option_category_menu_life:
-                endpoint += "?category=life";
-                break;
-            case R.id.option_category_menu_entertainment:
-                endpoint += "?category=entertainment";
-                break;
-            case R.id.option_category_menu_knowledge:
-                endpoint += "?category=knowledge";
-                break;
-            case R.id.option_category_menu_game:
-                endpoint += "?category=geme";
-                break;
-            case R.id.option_category_menu_fun:
-                endpoint += "?category=fun";
-                break;
-        }
-        BookmarksFetcher.get(endpoint, null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(JSONObject jObj) {
-                try {
-                    ArrayList<Entry> entries = new ArrayList<Entry>();
-                    JSONArray bookmarkJsons = jObj.getJSONArray("bookmarks");
-                    for (int i = 0; i < bookmarkJsons.length(); i++) {
-                        Entry entry = new Entry((JSONObject) bookmarkJsons.get(i));
-                        entries.add(entry);
-                    }
-                    entries = new ArrayList<Entry>(new LinkedHashSet<Entry>(entries));
-                    replaceAll(entries);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                feedResponseHandler.onSuccess();
-            }
-
-            @Override
-            public void onFinish() {
-                feedResponseHandler.onFinish();
-            }
-        });
+    protected String getEndPoint() {
+        return "entrylist";
     }
 }
