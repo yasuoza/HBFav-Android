2e5beb6f235b02011c8f3461eab860c1fe58a5c6
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-11-17 15:07:20 +0900

    Hatena account register view

--- HBFav/src/main/java/com/hbfav/android/ui/setting/UserRegistrationActivity.java
+++ HBFav/src/main/java/com/hbfav/android/ui/setting/UserRegistrationActivity.java
@@ -2,46 +2,57 @@ package com.hbfav.android.ui.setting;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.hbfav.R;
-import com.hbfav.android.Constants;
 import com.hbfav.android.core.UserInfoManager;
 
 public class UserRegistrationActivity extends Activity {
-    private EditText mEditTextUserName;
+    TextView mTextViewAccount;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_regstrasion);
 
-        mEditTextUserName =  (EditText) findViewById(R.id.edit_text_user_name);
-        mEditTextUserName.setText(UserInfoManager.getUserName());
+        mTextViewAccount = (TextView) findViewById(R.id.text_view_account);
+        mTextViewAccount.setText(UserInfoManager.getUserName());
+
+        findViewById(R.id.username_linear_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputUserNamePopUp();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_user_registration, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_save:
-                String userName = mEditTextUserName.getText().toString();
-                UserInfoManager.setUserName(userName);
-                this.finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    private void showInputUserNamePopUp() {
+        View promptsView = LayoutInflater.from(this).inflate(R.layout.prompt_edit_username, null);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(getString(R.string.setting_account));
+        alert.setView(promptsView);
+        final EditText input = (EditText) promptsView.findViewById(R.id.prompt_edit_username);
+        input.setText(UserInfoManager.getUserName());
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                if (input.getText() == null) {
+                    return;
+                }
+                UserInfoManager.setUserName(input.getText().toString());
+                mTextViewAccount.setText(UserInfoManager.getUserName());
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+        alert.show();
     }
 }
