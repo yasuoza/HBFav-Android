57e126d730865f5b67c5b7c4f900c7f5c93c57db
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-11-17 15:42:04 +0900

    Changed account settings from activity to fragment

--- HBFav/src/main/java/com/hbfav/android/ui/setting/SettingFragment.java
+++ HBFav/src/main/java/com/hbfav/android/ui/setting/SettingFragment.java
@@ -1,20 +1,28 @@
 package com.hbfav.android.ui.setting;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
 import android.app.ListFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.hbfav.R;
 import com.hbfav.android.Constants;
+import com.hbfav.android.core.UserInfoManager;
 import com.hbfav.android.ui.MainActivity;
 
-public class SettingFragment extends ListFragment {
+public class SettingFragment extends Fragment {
+    TextView mTextViewAccount;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -38,28 +46,38 @@ public class SettingFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_setting_view, container, false);
-
-        String[] menus = new String[]{
-                getString(R.string.setting_account),
-        };
-
-        setListAdapter(new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.setting_list_item,
-                android.R.id.text1,
-                menus
-        ));
+        mTextViewAccount = (TextView) rootView.findViewById(R.id.text_view_account);
+        mTextViewAccount.setText(UserInfoManager.getUserName());
+        rootView.findViewById(R.id.username_linear_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputUserNamePopUp();
+            }
+        });
         return rootView;
     }
 
-    @Override
-    public void onListItemClick(ListView listView, View view, int position, long id) {
-        switch (position) {
-            case 0:
-                startActivity(new Intent(getActivity(), UserRegistrationActivity.class));
-                break;
-            default:
-                break;
-        }
+    private void showInputUserNamePopUp() {
+        Context context = getActivity();
+        View promptsView = LayoutInflater.from(context).inflate(R.layout.prompt_edit_username, null);
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+        alert.setTitle(getString(R.string.setting_account));
+        alert.setView(promptsView);
+        final EditText input = (EditText) promptsView.findViewById(R.id.prompt_edit_username);
+        input.setText(UserInfoManager.getUserName());
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                if (input.getText() == null) {
+                    return;
+                }
+                UserInfoManager.setUserName(input.getText().toString());
+                mTextViewAccount.setText(UserInfoManager.getUserName());
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+        alert.show();
     }
 }
