6888d0de43053206069881ddb86abf1fc5a366a0
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-11-22 22:22:19 +0900

    CreditsActivity have back button

--- HBFav/src/main/java/com/hbfav/android/ui/about/CreditsActivity.java
+++ HBFav/src/main/java/com/hbfav/android/ui/about/CreditsActivity.java
@@ -1,8 +1,10 @@
 package com.hbfav.android.ui.about;
 
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.hbfav.R;
 
@@ -14,6 +16,18 @@ public class CreditsActivity extends Activity {
 
         setContentView(R.layout.activity_credits);
 
-        getActionBar().setTitle(getString(R.string.about_credits));
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle(getString(R.string.about_credits));
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
