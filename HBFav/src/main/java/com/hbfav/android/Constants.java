0e9873b464617defc46fe58125baa3be12e2252e
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-11-25 19:31:34 +0900

    i18n for :jp:

--- HBFav/src/main/java/com/hbfav/android/Constants.java
+++ HBFav/src/main/java/com/hbfav/android/Constants.java
@@ -29,5 +29,5 @@ public class Constants {
 
     public static final String[] CATEGORIES = MainActivity.getContextOfApplication()
         .getResources()
-        .getStringArray(R.array.entry_category_list);
+        .getStringArray(R.array.params_entry_category);
 }
