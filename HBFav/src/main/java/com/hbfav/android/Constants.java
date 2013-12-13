85da72e0fd34191e083b84b10b396a6d800f6653
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-12-13 15:47:19 +0900

    Change Constants static variables -> static methods

--- HBFav/src/main/java/com/hbfav/android/Constants.java
+++ HBFav/src/main/java/com/hbfav/android/Constants.java
@@ -11,8 +11,6 @@ public class Constants {
      */
     public static final String ARG_SECTION_NUMBER = "section_number";
 
-    public static final String PREF_USER_NAME = MainActivity.getContextOfApplication().getString(R.string.pref_hatena_user_name);
-
     public static final String HBFAV_BASE_URL = "http://hbfav-android.herokuapp.com/";
 
     public static final String BASE_THUMBNAIL_URL = "http://www.st-hatena.com/users/";
@@ -25,31 +23,48 @@ public class Constants {
     public static final String HATENA_API_KEY = "";
     public static final String HATENA_API_KEY_SECRET = "";
 
-    public static final NavigationItem[] MENUS = new NavigationItem[]{
-            new NavigationItem(
-                    null,
-                    MainActivity.getContextOfApplication().getString(R.string.title_section0)
-            ),
+    private static NavigationItem[] menues;
+    private static String[] categories;
 
-            new NavigationItem(
-                    MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_star),
-                    MainActivity.getContextOfApplication().getString(R.string.title_section1)
-            ),
-            new NavigationItem(
-                    MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_doc),
-                    MainActivity.getContextOfApplication().getString(R.string.title_section2)
-            ),
-            new NavigationItem(
-                    MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_heart),
-                    MainActivity.getContextOfApplication().getString(R.string.title_section3)
-            ),
-            new NavigationItem(
-                    MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_about),
-                    MainActivity.getContextOfApplication().getString(R.string.title_section4)
-            )
-    };
+    public static String getPrefUserName() {
+        return MainActivity.getContextOfApplication().getString(R.string.pref_hatena_user_name);
+    }
 
-    public static final String[] CATEGORIES = MainActivity.getContextOfApplication()
-            .getResources()
-            .getStringArray(R.array.params_entry_category);
+    public static NavigationItem[] getMenus() {
+        if (menues == null) {
+            menues = new NavigationItem[] {
+                    new NavigationItem(
+                            null,
+                            MainActivity.getContextOfApplication().getString(R.string.title_section0)
+                    ),
+
+                    new NavigationItem(
+                            MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_star),
+                            MainActivity.getContextOfApplication().getString(R.string.title_section1)
+                    ),
+                    new NavigationItem(
+                            MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_doc),
+                            MainActivity.getContextOfApplication().getString(R.string.title_section2)
+                    ),
+                    new NavigationItem(
+                            MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_heart),
+                            MainActivity.getContextOfApplication().getString(R.string.title_section3)
+                    ),
+                    new NavigationItem(
+                            MainActivity.getContextOfApplication().getResources().getDrawable(R.drawable.ic_about),
+                            MainActivity.getContextOfApplication().getString(R.string.title_section4)
+                    )
+            };
+        }
+        return menues;
+    }
+
+    public static String[] getCategories() {
+        if (categories == null) {
+            categories = MainActivity.getContextOfApplication()
+                    .getResources()
+                    .getStringArray(R.array.params_entry_category);
+        }
+        return categories;
+    }
 }
